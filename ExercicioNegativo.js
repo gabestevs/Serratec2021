194707eb3a3afcb14ded730a8800cfb1ec5b6441
gabestevs <g.gasparesteves@gmail.com>
2021-04-08 15:42:10 -0300

    Deletenado teste

--- ExercicioNegativo.js
+++
@@ -1,7 +0,0 @@
-const ler = require("prompt-sync")();
-
-var divida = ler("Digite o valor da sua divida em numeros negativos: ");
-
-divida = Math.abs(divida);
-
-console.log("O valor do emprestimo será de: ", divida);
