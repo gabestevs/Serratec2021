3ee582a861cedd591d97eea4182848c23d50065e
gabestevs <g.gasparesteves@gmail.com>
2021-04-10 23:29:00 -0300

    Exercicios das Fases de 0 até a 8.

--- Fase8/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-let nome = ler('~> ');
-    console.log('Digite o dia do compromisso');
-    let dia = parseInt(ler('~> '));
-    console.log('Digite uma descrição para o compromisso');
-    let desc = ler('~> ');
